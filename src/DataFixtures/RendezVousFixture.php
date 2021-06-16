0a7806c41714bc198789333d0875ceb70617c6e7
CAMPAGNE Alexandre <campagne.a@hotmail.fr>
2021-06-16 15:45:17 +0200

    fixture RendezVous

--- src/DataFixtures/RendezVousFixture.php
+++ src/DataFixtures/RendezVousFixture.php
@@ -16,20 +16,41 @@ class RendezVousFixture extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-        // for ($i = 0; $i < 5; $i++) {
-        //     $patient = new Patient();
-        //     $doctor = new Doctor();
-        //     $cabinet = new Cabinet();
+        $patient = (new Patient())
+            ->setNom("Pavard")
+            ->setPrenom("Benjamin")
+            ->setDateNaissance(date_create('1999-10-10'))
+            ->setRue("Rue")
+            ->setNumeroRue(666)
+            ->setVille("Lille")
+            ->setCodePostal(59000)
+            ->setMail("kiki@kuku.fr")
+            ->setTelephone(0606060606);
 
-        //     $rdv = new RendezVous();
-        //     $rdv->setDate(date_create('1999-10-1' . $i));
-        //     $rdv->setPatientId($i);
-        //     $rdv->setDoctorId($i);
-        //     $rdv->setCabinetId($cabinet->getId());
+        $manager->persist($patient);
 
-        //     $manager->persist($rdv);
-        // }
+        $doctor = (new Doctor())
+            ->setFirstName("Namour")
+            ->setLastName("Maboule");
 
-        // $manager->flush();
+        $manager->persist($doctor);
+
+        $cabinet = (new Cabinet())
+            ->setStreet("Boulevard")
+            ->setNumber(999)
+            ->setCity("Roubaix")
+            ->setPostal(59999);
+
+        $manager->persist($cabinet);
+
+        $rdv = new RendezVous();
+        $rdv->setDate(date_create('1999-10-11'));
+        $rdv->setPatientId($patient);
+        $rdv->setDoctorId($doctor);
+        $rdv->setCabinetId($cabinet);
+
+        $manager->persist($rdv);
+
+        $manager->flush();
     }
 }
